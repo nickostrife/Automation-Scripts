85aafe854136493c2b15fc144223af8ca24a48de
Nicko <40533459+nickostrife@users.noreply.github.com>
2020-10-23 22:35:14 +0700

    Revert "Add get sheets feature"
    This reverts commit a844181b638f9509778c23350b27e87b9e4ec435.

--- excel-sample.py
+++ excel-sample.py
@@ -3,10 +3,4 @@ import os
 from pathlib import Path
 
 wb = openpyxl.load_workbook(Path('C:/Users/Nicko/Documents/automate_online-materials/example.xlsx'))
-print(wb.sheetnames)
-sheet = wb['Sheet3']
-print(sheet)
-print(type(sheet))
-print(sheet.title)
-anotherSheet = wb.active
-print(anotherSheet)
+print(type(wb))
