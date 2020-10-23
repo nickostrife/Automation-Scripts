6e853f9d0da07b830d7ccd19677a370fdc2a3807
Nicko <40533459+nickostrife@users.noreply.github.com>
2020-10-23 22:51:57 +0700

    Commit changes to check-value

--- excel-sample.py
+++ excel-sample.py
@@ -3,10 +3,19 @@ import os
 from pathlib import Path
 
 wb = openpyxl.load_workbook(Path('C:/Users/Nicko/Documents/automate_online-materials/example.xlsx'))
-print(wb.sheetnames)
-sheet = wb['Sheet3']
-print(sheet)
-print(type(sheet))
-print(sheet.title)
-anotherSheet = wb.active
-print(anotherSheet)
+# print(wb.sheetnames)
+# sheet = wb['Sheet3']
+# print(sheet)
+# print(type(sheet))
+# print(sheet.title)
+# anotherSheet = wb.active
+# print(anotherSheet)
+
+sheet = wb['Sheet1']
+print("sheet['A1'] = {}".format(sheet['A1']))
+print("sheet['A1'].value = {}".format(sheet['A1'].value))
+c = sheet['B1']
+print("c.value = {}".format(c.value))
+print('Row {}, Column {} is {}'.format(c.row, c.column, c.value))
+print("Cell {} is {}".format(c.coordinate, c.value))
+print("sheet['C1'].value = {}".format(sheet['C1'].value))
