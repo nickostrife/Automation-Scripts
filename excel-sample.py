a844181b638f9509778c23350b27e87b9e4ec435
Nicko <40533459+nickostrife@users.noreply.github.com>
2020-10-23 22:34:01 +0700

    Add get sheets feature
    added new feature

--- excel-sample.py
+++ excel-sample.py
@@ -3,4 +3,10 @@ import os
 from pathlib import Path
 
 wb = openpyxl.load_workbook(Path('C:/Users/Nicko/Documents/automate_online-materials/example.xlsx'))
-print(type(wb))
+print(wb.sheetnames)
+sheet = wb['Sheet3']
+print(sheet)
+print(type(sheet))
+print(sheet.title)
+anotherSheet = wb.active
+print(anotherSheet)
